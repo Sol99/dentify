b184763871ec5255c752e74aa74b49bab2dc0eea
Lautaro <lautyguasch10@gmail.com>
2022-08-24 21:34:17 -0300

    Se actualizan campos ABM tratamientos

--- src/models/Treatment.js
+++ src/models/Treatment.js
@@ -6,7 +6,7 @@ const TreatmentSchema = new Schema ({
     fecha: { type: Date, default: Date.now },
     caraDiente: { type: String, required: false },
     importe: { type: String, required: false },
-    matriculaOdontologo: {type: String, required: false },
+    matriculaOdontologo: {type: String, required: false, default: 30315 },
     paciente: {type: String},
     observaciones: {type: String},
     user: {type:String}
