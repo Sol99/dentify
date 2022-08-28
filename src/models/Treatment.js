0f0695365e5914f45a814ae8871b9f1167e27875
Lautaro <lautyguasch10@gmail.com>
2022-08-27 21:06:50 -0300

    se agrega seleccion de paciente, de cara del diente, y se asocia al codigo desde backend

--- src/models/Treatment.js
+++ src/models/Treatment.js
@@ -3,11 +3,12 @@ const { Schema } = mongoose;
 
 const TreatmentSchema = new Schema ({
     codigo: {type: Number},
+    nombre: {type: String},
     fecha: { type: Date, default: Date.now },
     caraDiente: { type: String, required: false },
     importe: { type: String, required: false },
     matriculaOdontologo: {type: String, required: false, default: 30315 },
-    paciente: {type: String},
+    paciente: {type: Array},
     observaciones: {type: String},
     user: {type:String}
 });
